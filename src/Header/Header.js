d16613b5908f6a97b16a1ea78d8a3b673dc97bd7
sovsianykov <swebproger@gmail.com>
2021-01-11 17:58:45 +0200

    f

---
+++ src/Header/Header.js
@@ -0,0 +1,13 @@
+import React,{Component} from "react";
+import Navb from "./Navbar/Navb";
+class Header extends Component{
+    render() {
+        return (
+            <div>
+                <Navb/>
+            </div>
+        )
+    }
+
+}
+export default Header
