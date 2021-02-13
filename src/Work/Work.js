ed11f8ff62db7c1dc06bf81cc9a9d8d5275cd04a
”sovsianykov” <swebproger@gmail.com>
2021-02-13 17:01:08 +0200

    compressed 5

--- src/Work/Work.js
+++ src/Work/Work.js
@@ -2,8 +2,12 @@ import React,{Component} from "react";
 
 
 import bluestar from  '../Img/items/bluestardemo.png'
-import extra from  '../Img/items/extraordinary.jpg'
+import extra from  '../Img/items/extraordinary.png'
 import search from  '../Img/items/search.png'
+import player from  '../Img/items/myplayer.png'
+import pills from  '../Img/items/u-bills.png'
+import present from  '../Img/items/presennt-simple.png'
+
 
 
 
@@ -22,6 +26,19 @@ class Work extends Component {
                             Check out some of my projects
                         </p>
                         <div className="items">
+                        <div className="item">
+                                <a href="https://my-demo-payer.netlify.app/">
+                                <div className="item-image">
+                                    <img src={player} alt=""/>
+                                </div>
+                                <div className="item-text">
+                                    <div className="item-text-wrap">
+                                        <p className="item-text-category">React app</p>
+                                        <h2 className="item-text-title">My Player</h2>
+                                    </div>
+                                </div>
+                                </a>
+                            </div>
                             <div className="item">
                                 <a href="https://wesbound1.github.io/Serhii_OvsianykovDemo/">
                                 <div className="item-image">
@@ -45,7 +62,7 @@ class Work extends Component {
                                 </div>
                                 <div className="item-text">
                                     <div className="item-text-wrap">
-                                        <p className="item-text-category">UI/UX</p>
+                                        <p className="item-text-category">Web-site</p>
                                         <h2 className="item-text-title">World Experience</h2>
                                     </div>
                                 </div>
@@ -59,7 +76,33 @@ class Work extends Component {
                                 <div className="item-text">
                                     <div className="item-text-wrap">
                                         <p className="item-text-category">js app</p>
-                                        <h2 className="item-text-title">Vanishing</h2>
+                                        <h2 className="item-text-title">Search Te word</h2>
+                                    </div>
+                                </div>
+                                </a>
+                            </div>
+                            <div className="item">
+                                <a href="https://communalca-3.netlify.app/">
+                                <div className="item-image">
+                                    <img src={pills} alt=""/>
+                                </div>
+                                <div className="item-text">
+                                    <div className="item-text-wrap">
+                                        <p className="item-text-category">React app</p>
+                                        <h2 className="item-text-title">u-bills</h2>
+                                    </div>
+                                </div>
+                                </a>
+                            </div>
+                            <div className="item">
+                                <a href="https://present-siple.netlify.app/">
+                                <div className="item-image">
+                                    <img src={present} alt=""/>
+                                </div>
+                                <div className="item-text">
+                                    <div className="item-text-wrap">
+                                        <p className="item-text-category">React app</p>
+                                        <h2 className="item-text-title">Present Simple tense</h2>
                                     </div>
                                 </div>
                                 </a>
